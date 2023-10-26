a8ec36a9828ecc27d80ccb548be64ddcc16fee25
Pietro Galliani <pgallian@gmail.com>
2023-10-26 13:31:27 +0200

    example of calling swipl from python

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 13:27:26 2023
+
+@author: pgalliani
+"""
+
+from pyswip import Prolog
+
+prolog = Prolog()
+
+prolog.consult("examples/grandparent.pl")
