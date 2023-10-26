0993e6d8302d496fd39a3c01a45114f9a69500ba
Pietro Galliani <pgallian@gmail.com>
2023-10-26 17:12:50 +0200

    Using pyswip to communicate with Metagol, load metarules from different files

---
+++ metarules.pl
@@ -0,0 +1,2 @@
+metarule([P,Q], [P,A,B], [[Q,A,B]]).
+metarule([P,Q,R], [P,A,B], [[Q,A,C],[R,C,B]]).
--- test.py
+++ test.py
@@ -7,6 +7,12 @@ Created on Thu Oct 26 13:27:26 2023
 
 from pyswip import Prolog
 
+
 prolog = Prolog()
 
-prolog.consult("examples/grandparent.pl")
+prolog.consult("metagol.pl")
+prolog.consult("metarules.pl")
+
+prolog.consult("examples/grandparent2.pl")
+
+
